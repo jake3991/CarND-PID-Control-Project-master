b3c2b0fbdc00b01daea94963d274ad3d9ffd8a78
jake3991 <john.mcconnell.1874@gmail.com>
2017-11-03 19:16:26 -0700

    Change

---
+++ src/PID.cpp
@@ -0,0 +1,32 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+    PID::Kp = Kp;
+    PID::Ki = Ki;
+    PID::Kd = Kd;
+    p_error = d_error = i_error = 0.0;
+
+}
+
+void PID::UpdateError(double cte) {
+
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error += cte;
+
+}
+
+double PID::TotalError() {
+    return 0.0;
+}
+
